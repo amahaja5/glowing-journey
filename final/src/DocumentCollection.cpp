7d2098c992df8ee5801de1ab1fef045dd09c1cbc
Avi Mahajan <mahajan.avi@gmail.com>
2016-12-07 20:05:30 -0500

    Updated Makefile to make sure that make clean actually cleans what we need to clean, and that make creates all the test files

--- final/src/DocumentCollection.cpp
+++ final/src/DocumentCollection.cpp
@@ -12,7 +12,6 @@
 #include <iostream>
 #include <fstream>
 
-std::map<std::vector<std::string>, int> freq; // apologies for this, if this is in our class file, it returns
                                               // _M_constructor null error
 void DocumentCollection :: pushBackDoc(std::pair<std::string,NgramCollection> docNgram) {
 	docVector.push_back(docNgram);
